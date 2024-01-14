f40879570dc7b5918c46fb9babdeb15ad7380f1f
CasimiroGómez <casimiro.gomez@smartest.mx>
2024-01-14 10:17:41 -0600

    Creacion de shared

---
+++ src/app/shared/indicators/spinner/spinner.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from './spinner.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, SpinnerComponent
+  ]
+})
+export class SpinnerModule { }
---
+++ src/app/shared/popups/files-upload/files-upload.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilesUploadComponent } from './files-upload.component';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule, 
+    FilesUploadComponent
+  ]
+})
+export class FilesUploadModule { }
