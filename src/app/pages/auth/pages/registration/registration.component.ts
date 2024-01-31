9bf810abde4361d0396761e6575e43eab2859ff8
CasimiroGómez <casimiro.gomez@smartest.mx>
2024-01-31 11:20:02 -0600

    captura de nivel de usuario

--- src/app/pages/auth/pages/registration/registration.component.ts
+++ src/app/pages/auth/pages/registration/registration.component.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 import { Puesto } from '@app/models/backend/puesto';
 import { AreaTrabajo } from '@app/models/backend/area';
 import { ThemePalette } from '@angular/material/core';
+import { MatRadioChange } from '@angular/material/radio';
 
 
 @Component({
@@ -28,12 +29,20 @@ export class RegistrationComponent implements OnInit {
     {name:"Cirujano"},
     {name:"Anestesiologo"},
   ];
-  labelPosition = ['usuario' , 'staff' , 'administrador' , 'superusuario'];
-  checked = false;
-  indeterminate = false;
-  disabled = false;
+  permisos = [
+    'usuario' , 
+    'staff', 
+    'administrador', 
+    'superusuario'
+  ];
+
   puestoElegido !: Puesto;
   areaElegida !: AreaTrabajo;
+  selectedRadio : string = '';
+  is_active : boolean = false;
+  is_staff : boolean = false;
+  is_admin : boolean = false;
+  is_superuser : boolean = false;
 
 
   constructor(private store: Store<fromRoot.State>) {
@@ -55,10 +64,43 @@ export class RegistrationComponent implements OnInit {
       nombre: valor
 
     }
+    
     console.log(valor);
     //this.areaElegida = valor;
 }
-
+radioButtonChange(data: MatRadioChange) {
+  switch (this.selectedRadio)
+  {
+      case this.permisos[0]:
+          this.is_active  = true;
+          this.is_staff = false;
+          this.is_admin = false;
+          this.is_superuser = false;
+        break;
+      case this.permisos[1]:
+          this.is_active  = false;
+          this.is_staff = true;
+          this.is_admin = false;
+          this.is_superuser = false;
+        break;
+        case this.permisos[2]:
+          this.is_active  = false;
+          this.is_staff = false;
+          this.is_admin = true;
+          this.is_superuser = false;
+        break;
+        case this.permisos[3]:
+          this.is_active  = false;
+          this.is_staff = false;
+          this.is_admin = false;
+          this.is_superuser = true;
+        break;
+    default:
+        alert('Default case');
+        break;
+  }
+  console.log(this.selectedRadio)
+}
   registrarUsuario(form: NgForm){
     const userRegistrationRequest: fromUser.UserCreateRequest = {
       nombre: form.value.nombre,
@@ -75,10 +117,10 @@ export class RegistrationComponent implements OnInit {
       area : this.areaElegida,
       empresa_id : form.value.empresa,
       numeroEmpledo: form.value.noempleado,
-      is_admin : false,
-      is_active : true,
-      is_staff : true,
-      is_superadmin : false,
+      is_admin : this.is_admin,
+      is_active : this.is_active,
+      is_staff : this.is_staff,
+      is_superadmin : this.is_superuser,
 
     };
     console.log(userRegistrationRequest);
