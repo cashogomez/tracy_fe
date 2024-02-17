f7a10b10c98cc0a4dbe62e6d6b952df08cbb3fc1
CasimiroGómez <casimiro.gomez@smartest.mx>
2024-02-17 17:25:18 -0600

    Página de lavado manual

--- src/app/pages/static/pages/empaqueotrasareas/empaqueotrasareas.module.ts
+++ src/app/pages/static/pages/empaqueotrasareas/empaqueotrasareas.module.ts
@@ -3,6 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { EmpaqueotrasareasRoutingModule } from './empaqueotrasareas-routing.module';
 import { EmpaqueotrasareasComponent } from './empaqueotrasareas.component';
+import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDivider } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterOutlet, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -11,7 +25,28 @@ import { EmpaqueotrasareasComponent } from './empaqueotrasareas.component';
   ],
   imports: [
     CommonModule,
-    EmpaqueotrasareasRoutingModule
-  ]
+    EmpaqueotrasareasRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatDivider,
+    MatIcon,
+    MatTooltipModule,
+    MatButtonModule,
+    MatOptionModule,
+    MatSelectModule,
+
+    RouterOutlet,
+    RouterLink, 
+    RouterLinkActive,
+    RouterModule,
+  ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
+    provideMomentDateAdapter(),
+  ],
 })
 export class EmpaqueotrasareasModule { }
--- src/app/pages/static/pages/empaquequirofano/empaquequirofano.component.ts
+++ src/app/pages/static/pages/empaquequirofano/empaquequirofano.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepickerIntl } from '@angular/material/datepicker';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { AreaTrabajo } from '@app/models/backend/area';
+import { NotificationService } from '@app/services';
+import 'moment/locale/es';
 
 @Component({
   selector: 'app-empaquequirofano',
@@ -8,4 +16,147 @@ import { Component } from '@angular/core';
 })
 export class EmpaquequirofanoComponent {
 
+  editarRegistro !: Element;
+  borrarRegistro !: Element;
+  area: AreaTrabajo[]=[
+  {
+    id: 1,
+    tipo: 'interna',
+    nombre: 'Quirófano'
+  },
+  {
+    id: 2,
+    tipo: 'interna',
+    nombre: 'Urgencia'
+  },
+  {
+    id: 3,
+    tipo: 'interna',
+    nombre: 'Ceye'
+  },
+];
+setmaterial =[
+  {
+    nombre: 'Pinzas',
+  },
+  {
+    nombre: 'Set Instrumental',
+  },
+  {
+    nombre: 'set 2',
+  },
+];
+
+  /** Constants used to fill up our data base. */
+ ELEMENT_DATA: Element[] = [
+  {  nombre: 'H', prioridad: 'alta', estatus: 'falla', por_elaborar: 1  },
+  {  nombre: 'H', prioridad: 'media', estatus: 'falla', por_elaborar: 2 },
+  { nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 1  },
+  { nombre: 'H', prioridad: 'alta', estatus: 'falla', por_elaborar: 3  },
+  { nombre: 'H' , prioridad: 'alta', estatus: 'falla', por_elaborar: 3 },
+  { nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 2},
+  {  nombre: 'H' , prioridad: 'media', estatus: 'falla', por_elaborar: 2 },
+  { nombre: 'H' , prioridad: 'alta', estatus: 'falla', por_elaborar: 1 },
+  { nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 4 },
+  { nombre: 'H', prioridad: 'media', estatus: 'falla', por_elaborar: 1  },
+  {  nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 3  },
+  {  nombre: 'H', prioridad: 'media', estatus: 'falla', por_elaborar: 2  },
+  { nombre: 'H', prioridad: 'alta', estatus: 'falla', por_elaborar: 5  },
+  { nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 2 },
+  {  nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 2  },
+  {  nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 2 },
+  {  nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 1 },
+  {  nombre: 'H' , prioridad: 'alta', estatus: 'falla', por_elaborar: 4},
+  { nombre: 'H' , prioridad: 'media', estatus: 'falla', por_elaborar: 2},
+  {  nombre: 'H' , prioridad: 'media', estatus: 'falla', por_elaborar: 3 },
+];
+  areaElegida!: AreaTrabajo;
+  area_id!: number;
+
+// EliminarElementoTabla(key: number) {
+//   this.ELEMENT_DATA.forEach((value,index)=>{
+//       if(value.recepcion==key) this.ELEMENT_DATA.splice(index,1);
+//   });
+// } 
+
+  constructor(private notification: NotificationService,
+
+    
+    private _adapter: DateAdapter<any>,
+    private _intl: MatDatepickerIntl,
+    @Inject(MAT_DATE_LOCALE) private _locale: string,) { // Assign the data to the data source for the table to render
+      this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+
+  }
+
+  ngOnInit() {
+    this.updateCloseButtonLabel('Cerrar Calendario');
+  }
+
+  french() {
+    this._locale = 'es-ES';
+    this._adapter.setLocale(this._locale);
+    this.updateCloseButtonLabel('Cerrar el calendario');
+  }
+
+  updateCloseButtonLabel(label: string) {
+    this._intl.closeCalendarLabel = label;
+    this._intl.changes.next();
+  }
+
+  getDateFormatString(): string {
+    if (this._locale === 'es-ES') {
+      return 'DD/MM/AAAA';
+    } else if (this._locale === 'fr') {
+      return 'DD/MM/YYYY';
+    }
+    return '';
+  }
+  downloadPdf() {
+
+  }
+
+  // ************************ PAGINATOR *******************
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  //dataSource: MatTableDataSource<Element>;
+  dataSource: MatTableDataSource<Element>;
+// **********************************************************
+
+  displayedColumns = ['prioridad', 'nombre',  'por_elaborar', 'estatus', 'accion', 'impresion' ];
+
+  editarFila(element: Element) {
+    this.editarRegistro=element;
+  }
+  eliminarFila(element: Element) {
+    this.borrarRegistro=element;
+  }
+  changeArea(valor: AreaTrabajo) { 
+    this.areaElegida = valor;
+    this.area_id = this.areaElegida.id;
 }
+changeSetmaterial(valor: {nombre: string}) { 
+  console.log(valor);
+}
+}
+
+
+export interface Element {
+  prioridad: string;
+  nombre: string;
+  por_elaborar: number;
+  estatus: string;
+}
+
