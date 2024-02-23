eb6a2a0c8fa56b5897170d6cd1f63b00cee49b84
CasimiroGómez <casimiro.gomez@smartest.mx>
2024-02-22 22:56:54 -0600

    impresion

--- src/app/pages/static/pages/empaquequirofano/empaquequirofano.component.ts
+++ src/app/pages/static/pages/empaquequirofano/empaquequirofano.component.ts
@@ -7,6 +7,10 @@ import { MatTableDataSource } from '@angular/material/table';
 import { AreaTrabajo } from '@app/models/backend/area';
 import { NotificationService } from '@app/services';
 import 'moment/locale/es';
+import pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-empaquequirofano',
@@ -48,28 +52,45 @@ setmaterial =[
 ];
 
   /** Constants used to fill up our data base. */
- ELEMENT_DATA: Element[] = [
-  {  nombre: 'H', prioridad: 'alta', estatus: 'falla', por_elaborar: 1  },
-  {  nombre: 'H', prioridad: 'media', estatus: 'falla', por_elaborar: 2 },
-  { nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 1  },
-  { nombre: 'H', prioridad: 'alta', estatus: 'falla', por_elaborar: 3  },
-  { nombre: 'H' , prioridad: 'alta', estatus: 'falla', por_elaborar: 3 },
-  { nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 2},
-  {  nombre: 'H' , prioridad: 'media', estatus: 'falla', por_elaborar: 2 },
-  { nombre: 'H' , prioridad: 'alta', estatus: 'falla', por_elaborar: 1 },
-  { nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 4 },
-  { nombre: 'H', prioridad: 'media', estatus: 'falla', por_elaborar: 1  },
-  {  nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 3  },
-  {  nombre: 'H', prioridad: 'media', estatus: 'falla', por_elaborar: 2  },
-  { nombre: 'H', prioridad: 'alta', estatus: 'falla', por_elaborar: 5  },
-  { nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 2 },
-  {  nombre: 'H', prioridad: 'baja', estatus: 'falla', por_elaborar: 2  },
-  {  nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 2 },
-  {  nombre: 'H' , prioridad: 'baja', estatus: 'falla', por_elaborar: 1 },
-  {  nombre: 'H' , prioridad: 'alta', estatus: 'falla', por_elaborar: 4},
-  { nombre: 'H' , prioridad: 'media', estatus: 'falla', por_elaborar: 2},
-  {  nombre: 'H' , prioridad: 'media', estatus: 'falla', por_elaborar: 3 },
-];
+  ELEMENT_DATA = [
+    {Prioridad: 'baja', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'baja', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'media', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'baja', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'media', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'media', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'media', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+    {Prioridad: 'alta', Nombre: 'Set de Angiocardio', Elaborar:1, Estatus: 'Pendiente'},
+  
+  ];
+
   areaElegida!: AreaTrabajo;
   area_id!: number;
 
@@ -112,8 +133,8 @@ setmaterial =[
     }
     return '';
   }
-  downloadPdf() {
-
+  generarPDF() {
+    createPDF(this.ELEMENT_DATA, this.nombrejefa);
   }
 
   // ************************ PAGINATOR *******************
@@ -134,9 +155,9 @@ setmaterial =[
   //dataSource: MatTableDataSource<Element>;
   dataSource: MatTableDataSource<Element>;
 // **********************************************************
-
-  displayedColumns = ['prioridad', 'nombre',  'por_elaborar', 'estatus', 'accion', 'impresion' ];
-
+nombrejefa= "María Dolores Rodríguez Ramírez";
+  displayedColumns: string[] = ['Prioridad', 'Nombre', 'Elaborar', 'Estatus','Accion', 'Impresion'];
+  
   editarFila(element: Element) {
     this.editarRegistro=element;
   }
@@ -154,9 +175,194 @@ changeSetmaterial(valor: {nombre: string}) {
 
 
 export interface Element {
-  prioridad: string;
-  nombre: string;
-  por_elaborar: number;
-  estatus: string;
+  Prioridad: string;
+  Nombre: string;
+  Elaborar: number;
+  Estatus: string;
 }
 
+// ******************************* Impresión *****************************+
+const date = new Date();const año = date.getFullYear();const mes = date.toLocaleString('default', { month: 'short' });const mes2 = date.toLocaleString('default', { month: 'long' });const dia = date.getDate(); const hora = date.getHours();const minutos = date.getMinutes();
+const fecha =dia +' de '+ mes2 +' del '+ año +', '+ hora + ':' + minutos + 'hrs';
+
+const area = 'Quirófano'
+const tipo= 'Set de Instrumental';
+function buildTableBody(data: { [x: string]: { toString: () => any; }; }[], columns: (string | number)[]) {
+  var body: any[][] = [];
+
+
+  data.forEach(function(row: { [x: string]: { toString: () => any; }; }) {
+      var dataRow: any[] = [];
+
+      columns.forEach(function(column: string | number) {
+          dataRow.push({text : row[column].toString(), alignment : 'center', color : 'black', bold:false, fontSize: 9, margin: [0, 10, 0, 0]});
+      })
+
+      body.push(dataRow, );
+  });
+
+  return body;
+}
+
+function table(data: { [x: string]: { toString: () => any; }; }[] | { name: string; age: number; }[], columns: (string | number)[]) {
+  return {
+    style: 'tableExample',
+      table: {
+        widths: ['25%','25%', '25%', '25%'],
+          body: buildTableBody(data, columns),
+      },layout: 'noBorders'
+    
+  };
+}
+function getBase64ImageFromURL(url: string) {
+  return new Promise((resolve, reject) => {
+    var img = new Image();
+    img.setAttribute("crossOrigin", "anonymous");
+  
+    img.onload = () => {
+      var canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+  
+      var ctx = canvas.getContext("2d");
+      ctx!.drawImage(img, 0, 0);
+  
+      var dataURL = canvas.toDataURL("image/png");
+  
+      resolve(dataURL);
+    };
+  
+    img.onerror = error => {
+      reject(error);
+    };
+  
+    img.src = url;
+  });}
+
+  async function createPDF(dataSource:  { [x: string]: { toString: () => any; }; }[], nombreJefa: string){
+
+  const pdfDefinition: any = {
+
+    pageSize: 'letter',
+    pageMargins: [20, 270, 20, 80],
+
+    background: [
+      {
+        "image":"logo",
+          width: 100,
+          margin:[20,22,0,0]
+      },
+
+      {
+        "image":"logo",
+          width: 600,
+          margin:[-2,60,0,0],
+          opacity: 0.1
+      }
+    ],
+
+    header:[
+   
+      {text: 'INSTITUTO NACIONAL DE CIENCIAS MÉDICAS NUTRICIÓN SALVADOR ZUBIRÁN', style: 'header2'},
+      {text: 'SUBDIRECCIÓN DE ENFERMERÍA: '+nombreJefa, style: 'header'},
+      {text: 'DEPARTAMENTO DE ENFERMERÍA', style: 'header'},
+      {text: 'CENTRAL DE EQUIPOS Y ESTERILIZACIÓN', style: 'header'},
+      {text: 'Fecha de Impresión: '+fecha, style: 'header3'},
+      {text: 'EMPAQUE MATERIAL DE QUIRÓFANO Y CEyE', style: 'header2'},
+      {
+        columns: [
+          {
+            text: 'Tipo: '+tipo, 
+            width:'40%',alignment: "center", margin:[0,25,0,0], fontSize: 11, bold:true,
+          },
+          {
+            text: 'Área: '+area,
+            width:'20%',alignment: "center", margin:[0,25,0,0], fontSize: 11, bold:true,
+          },
+          {
+            text: 'Rango de Fecha: '+/*aqui va la primera la variable de fecha del piker*/'10/08/23'+' - '+/*aqui va la segunda la variable de fecha del piker*/'16/12/23',
+            width:'40%',alignment: "center", margin:[0,25,0,0], fontSize: 11, bold:true,
+          },
+        ]
+      },
+
+      {
+        style: 'tableExample', margin:[20,20,20,0],
+        table: {
+          widths: ['25%','25%', '25%', '25%'],
+          body: [
+            ['Prioridad', 'Nombre', 'Por Elaborar', 'Estatus'],
+          ]
+        },layout: 'noBorders'
+      },
+      
+    ],
+
+    content: [
+      
+       table(dataSource, ['Prioridad', 'Nombre', 'Elaborar', 'Estatus'], ),
+  ],
+  
+images:{
+  
+  "logo" : await getBase64ImageFromURL(
+      "../../assets/generales/Logo_nutricion.png")
+    
+},
+  styles: {
+
+    header: {
+      fontSize: 11,
+      bold: true,
+      margin: [40, 7, 0, 0],
+      alignment: "center",
+      color: 'black',
+      position:'fixed',
+    },
+    header2: {
+      fontSize: 11,
+      bold: true,
+      margin: [40, 30, 0, 0],
+      alignment: "center",
+      color: 'black',
+      position:'fixed',
+     
+    },
+    header3: {
+      fontSize: 11,
+      bold: true,
+      margin: [0, 27, 30, 0],
+      alignment: "right",
+      color: 'black',
+      position:'fixed',
+    },
+    footer: {
+      fontSize: 10,
+      margin: [0, 20, 0, 0],
+      alignment: "center",
+      color: 'black',
+      position:'fixed',
+    },
+    tableExample: {
+      fontSize: 11,
+      bold: true,
+      margin: [0, -15, 0, 0],
+      alignment : 'center',
+      color: 'black'
+    },
+  
+  },
+  
+  footer:[
+    
+    {text: 'TRACY © '+año, style: 'footer'},
+  ]
+  }
+    
+  
+
+  const pdf =  pdfMake.createPdf(pdfDefinition);
+  //pdf.download('Reporte Empaque Material De Quirófano Y CEyE '+ dia + '/'+mes2+'/'+año + ' ('+ hora + '/'+ minutos + 'hr)');
+
+  pdf.open()  
+}
